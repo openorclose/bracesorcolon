213d92797411d576ad1a17afaa93ccdfaf4111ca
GitHub Action <action@github.com>
2021-08-05 03:13:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/openorclose/bracesorcolon/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/openorclose/bracesorcolon/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.openorclose.bracesorcolon.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.openorclose.bracesorcolon.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/openorclose/bracesorcolon/services/MyProjectService.kt
+++ src/main/kotlin/com/github/openorclose/bracesorcolon/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.openorclose.bracesorcolon.services
 
+import com.github.openorclose.bracesorcolon.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
